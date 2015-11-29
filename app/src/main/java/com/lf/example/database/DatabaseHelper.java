563f15ab612f71a6e3654102b356b18904ed2aec
krishnalalstha <krishna06318@gmail.com>
2015-11-29 22:05:18 +0545

    database exampe

--- app/src/main/java/com/lf/example/database/DatabaseHelper.java
+++ app/src/main/java/com/lf/example/database/DatabaseHelper.java
@@ -9,6 +9,21 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 public class DatabaseHelper extends SQLiteOpenHelper {
 
+    public static final String TBL_CONTACT = "contact";
+    public static final String CONTACT_ID = "contact_id";
+    public static final String CONTACT_NAME = "contact_name";
+    public static final String CONTACT_ADDRESS = "address";
+    public static final String GNEDER = "contact_gender";
+
+
+    public static final String CREATE_CONTACT_TABLE = "CREATE TABLE IF NOT EXISTS "
+            + TBL_CONTACT
+            + "(id INTEGER PRIMARY KEY AUTOINCREMENT,"
+            + CONTACT_ID + " String UNIQUE, "
+            + CONTACT_NAME + " TEXT, "
+            + CONTACT_ADDRESS + " TEXT, "
+            + GNEDER + " TEXT )";
+
 
     public DatabaseHelper(Context context) {
         super(context, "db_contact", null, 1);
@@ -16,6 +31,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_CONTACT_TABLE);
 
     }
 
